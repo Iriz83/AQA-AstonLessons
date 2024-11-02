c7a3a2b84e98b4cb671cc62dd828474bf2ac73f2
Iriz83 <irinazakha@gmail.com>
2024-11-02 14:03:00 +0300

    Lesson1 add printThreeWords

--- Lesson1/Main.java
+++ Lesson1/Main.java
@@ -1,18 +1,25 @@
 package Lesson1;
 public class Main {
     public static void printThreeWords() {
-        System.out.print("Orange");
-        System.out.print(", ");
-        System.out.print("Banana");
-        System.out.print(", ");
+        System.out.println("Orange");
+        System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 55;
+        int b = -15;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 }
-
-
-
-
