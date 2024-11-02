04548620d9f9bfc752a5c0f2a2aa27e6104e1061
Iriz83 <irinazakha@gmail.com>
2024-11-02 13:31:26 +0300

    Lesson1 add printThreeWords

---
+++ Lesson1/Main.java
@@ -0,0 +1,18 @@
+package Lesson1;
+public class Main {
+    public static void printThreeWords() {
+        System.out.print("Orange");
+        System.out.print(", ");
+        System.out.print("Banana");
+        System.out.print(", ");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
+
+
+
+
